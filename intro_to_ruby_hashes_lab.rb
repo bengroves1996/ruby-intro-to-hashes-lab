0ace771ef43b4f6b177c95f2238a9f15d55d642e
bengroves <bengroves15@gmail.com>
2020-10-25 18:59:44 -0400

    commit tree 10e9502c5f12af9d180c2677bc02f721b0d60ee6

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,63 @@
 def new_hash
-
+	hash1 = {}
+	return hash1
 end
 
 def actor
-
+	actors = {name:  "Dwayne The Rock Johnson"}
+	return actors
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+		:railroads => {}}
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:names => {},
+			:rent_in_dollars => {}
+		} #closer railroad brace
+	} #closer curly brace
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:names => { reading_railroad: {},
+						pennsylvania_railroad: {},
+						b_and_o_railroad: {},
+						shortline: {}
+					}, #closer for names
+			:rent_in_dollars => {
+				one_piece_owned: 25,
+				two_pieces_owned: 50,
+				three_pieces_owned: 100,
+				four_pieces_owned: 200
+			} #closer for rent_in_dollars
+		} #closer railroad brace
+	} #closer curly brace
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
-end
+	monopoly = {
+		:railroads => {
+			:pieces => 4,
+			:names => { :reading_railroad => {"mortgage_value" => "$100"},
+						:pennsylvania_railroad => {"mortgage_value" => "$200"},
+						:b_and_o_railroad => {"mortgage_value" => "$400"},
+						:shortline => {"mortgage_value" => "$800"}
+					}, #closer for names
+			:rent_in_dollars => {
+				one_piece_owned: 25,
+				two_pieces_owned: 50,
+				three_pieces_owned: 100,
+				four_pieces_owned: 200
+			} #closer for rent_in_dollars
+		} #closer railroad brace
+	} #closer curly brace
+end 
